f9921aeaf00a8b25a1e4b3a5f82b52e06b87837b
Giovanna <giogarciatv13@gmail.com>
2025-08-24 23:37:56 -0600

    Mi primer commit-ejercicio1

--- ejercicio1.py
+++ ejercicio1.py
@@ -1,1 +1,8 @@
-aaa
+class Galleta:
+    def __init__(self, nombre, forma):
+        self.nombre=nombre
+        self.forma=forma
+
+    def Hornear(self):
+        print(f"Esta {self.nombre} ha sido horneada en forma de {self.forma}")
+        print("Buen Provecho!!😋")
